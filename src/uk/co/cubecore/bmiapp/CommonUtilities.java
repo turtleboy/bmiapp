c2268805f7aceea9d41ef46fe592b8cafb9494da
matthew womersley <matthew_womersley@yahoo.co.uk>
2013-01-26 16:29:58 +0000

    set server url and included senderID

--- src/uk/co/cubecore/bmiapp/CommonUtilities.java
+++ src/uk/co/cubecore/bmiapp/CommonUtilities.java
@@ -31,12 +31,12 @@ public final class CommonUtilities {
     /**
      * Base URL of the Demo Server (such as http://my_host:8080/gcm-demo)
      */
-    static final String SERVER_URL = null;
+    static final String SERVER_URL = "http://127.0.0.1/gcm-demo";
 
     /**
      * Google API project id registered to use GCM.
      */
-    static final String SENDER_ID = null;
+    static final String SENDER_ID = "598080744593";
 
     /**
      * Tag used on log messages.
